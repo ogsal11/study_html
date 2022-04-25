f713d636061599ca77a4bed160ff579982bacae5
de1bo <ogsal5765@naver.com>
2022-04-25 22:43:13 +0900

    first commit

---
+++ public/Directory & Listing Website Template - Listed/HTML/js/script.js
@@ -0,0 +1,63 @@
+/*
+
+Script  : Main JS
+Version : 1.0
+Author  : Surjith S M
+URI     : http://themeforest.net/user/surjithctly
+
+Copyright © All rights Reserved
+Surjith S M / @surjithctly
+
+*/
+
+
+$(function() {
+
+    "use strict";
+
+
+    /* ================================================
+      Google Map
+      ================================================ */
+
+
+    $(".map-icon").on('click', function() {
+        $(".map-fix").toggle();
+    });
+
+    $(".map-close").on('click', function() {
+        $(".map-fix").hide();
+    });
+
+});
+
+
+// Want to customize colors? go to snazzymaps.com
+function myMap() {
+    var maplat = $('#map').data('lat');
+    var maplon = $('#map').data('lon');
+    var mapzoom = $('#map').data('zoom');
+    // Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: maplat,
+            lng: maplon
+        },
+        zoom: mapzoom,
+        scrollwheel: false,
+        styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }],
+    });
+    var marker = new google.maps.Marker({
+        position: {
+            lat: maplat,
+            lng: maplon
+        },
+        map: map,
+        title: 'We are here!'
+    });
+}
+
+
+/*
+ * // End $ Strict Function
+ * ------------------------ */
